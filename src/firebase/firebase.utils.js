ec0b74b6ecbb4dec1716c40a15449caad70db7c8
adrianoduque97 <adrian.duque@capmation.com>
2020-07-22 22:22:05 -0500

    Firestore users documents

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,33 @@ var firebaseConfig = {
     measurementId: "G-P628ZLCXHZ"
   };
 
+  export const createUserProfileDocument = async (userAuth, additionalData) =>{
+    if(!userAuth) return;
+
+    const userRef = (firestore.doc(`users/${userAuth.uid}`))
+    const snapShot = await  userRef.get()
+
+    if(!snapShot.exists){
+      const {displayName, email} = userAuth
+      const createdAt = new Date()
+
+      try{
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+
+      }catch(e){
+        console.log(e.message)
+
+      }
+
+    }
+    return userRef
+  }
+
 firebase.initializeApp(firebaseConfig)
 
 export const auth = firebase.auth()
